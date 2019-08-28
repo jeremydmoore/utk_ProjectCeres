acef212b3ff9a3c86e5f1b0f0eecf90a8be37eb9
Jeremy Moore <alt.photosbyjeremy@gmail.com>
2019-08-28 15:26:17 -0400

    Clean-up respository

--- rename_pdf.ps1
+++
@@ -1,8 +0,0 @@
-# filename: rename_pdf.ps1
- # PowerShell script to rename multiple PDFs within a folder to a name that increments by 1
-
-# ask user for input
- $filename_stub = Read-Host "What is the new filename stub? i.e. 'CRLA_1995' without underscore or index number"
-
-# $variable.ToString("0000") will convert an integer into a string that is zero-padded to 4-spaces
- Dir *.pdf | ForEach-Object -begin { $count=1 } -process { rename-item $_ -NewName ($filename_stub + '_' + $count.ToString("0000") + '.pdf'); $count++ }
